5980e894c7f45ce7c618f33fd22a9bec86c2720c
Rebecca Young <moonbladez@hotmail.com>
2023-08-14 14:11:48 +0200

    added ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common';
 import { CoffeeModule } from './coffee/coffee.module';
 import { UserModule } from './user/user.module';
 import { IamModule } from './iam/iam.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoffeeModule, UserModule, IamModule],
+  imports: [CoffeeModule, UserModule, IamModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
