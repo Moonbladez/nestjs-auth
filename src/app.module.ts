7330bf0e5f4f9c6880e4161ae40e0609ba7b6511
Rebecca Young <moonbladez@hotmail.com>
2023-08-14 12:58:40 +0200

    Add IamModule and BcryptService for hashing

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { CoffeeModule } from './coffee/coffee.module';
 import { UserModule } from './user/user.module';
+import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [CoffeeModule, UserModule],
+  imports: [CoffeeModule, UserModule, IamModule],
 })
 export class AppModule {}
